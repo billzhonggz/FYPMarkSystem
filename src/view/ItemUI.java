93d39c88b506a9f49486ee0db5c179115a70fca5
Bill ZHONG <billzhonggz@outlook.com>
2017-04-24 09:46:30 +0800

    AddItem UI by Bruce shuai

--- src/view/ItemUI.java
+++ src/view/ItemUI.java
@@ -6,5 +6,17 @@ import javax.swing.*;
  * Created by admin on 2017/4/22.
  */
 public class ItemUI {
-    private JTextField textField1;
+    private JTextField item1Name;
+    private JTextField item1per;
+    private JTextField item2name;
+    private JPanel Jpanel2;
+    private JTextField item2per;
+    private JTextField item3name;
+    private JTextField item3per;
+    private JTextField item4name;
+    private JTextField item4per;
+    private JTextField item5name;
+    private JTextField item5per;
+    private JButton save;
+    private JButton cancel;
 }
